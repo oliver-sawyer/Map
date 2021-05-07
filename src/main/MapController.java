5678dfaa776d5086dd87b7dc774b11843b6b673d
oliver-sawyer <60905579+oliver-sawyer@users.noreply.github.com>
2021-05-07 16:40:49 -0500

    Added static map functionality

--- src/main/MapController.java
+++ src/main/MapController.java
@@ -1,4 +1,22 @@
 package main;
 
-public class MapController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MapController implements Initializable {
+    @FXML
+    ImageView map;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            map.setImage(HTTPSUtil.getStaticMap("Milwaukee,WI"));
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
