322b5c61a1028cd1bd8cb01cd3c5ae1475198937
Alex <alex_fuller@ymail.com>
2021-05-07 20:46:35 -0500

    -Added embedded web mapquest-core.js

--- src/main/MapController.java
+++ src/main/MapController.java
@@ -2,8 +2,11 @@ package main;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.web.WebView;
+import netscape.javascript.JSObject;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,16 +16,24 @@ public class MapController implements Initializable {
     ImageView map;
     @FXML
     TextField addressField;
+    @FXML
+    Slider zoomSlider;
+    @FXML
+    WebView webMap;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        //System.out.println(mapDoc.getMember("map"));
     }
 
     @FXML
     public void address() {
         try {
-            map.setImage(MapUtil.getStaticMap(addressField.getText()));
+            //map.setImage(MapUtil.getStaticMap(addressField.getText(), zoomSlider.getValue()));
+            webMap.getEngine().loadContent(MapUtil.getMapHtml(90,90,(int)zoomSlider.getValue()));
+            JSObject mapDoc = (JSObject) webMap.getEngine().getDocument();
+
         } catch(Exception e) {
             e.printStackTrace();
         }
--- src/main/MapUtil.java
+++ src/main/MapUtil.java
@@ -28,8 +28,53 @@ public class MapUtil {
             System.out.println(url.getHost());
             return url;
         } catch(MalformedURLException e) {
-            System.err.println("Incorrecte URL");
+            System.err.println("Incorrect URL");
             return null;
         }
+    }
+    public static Image getStaticMap(String address, double zoom) throws IOException {
+        HttpsURLConnection connection = HTTPSUtil.establishConnection(buildConnectionURL(address, zoom));
+        BufferedImage image = ImageIO.read(connection.getInputStream());
+        int status = connection.getResponseCode();
+        System.out.println(status);
+        return SwingFXUtils.toFXImage(image, null);
+    }
+
+    private static URL buildConnectionURL(String address, double zoom) {
+        try {
+            int intZoom = (int)zoom;
+            URL url =  new URL("https://www.mapquestapi.com/staticmap/v5/map?key=" + KEY + "&center=" + address +"&zoom="+intZoom+ "&size=1200,800@2x");
+            System.out.println(url.getHost());
+            return url;
+        } catch(MalformedURLException e) {
+            System.err.println("Incorrect URL");
+            return null;
+        }
+    }
+    public static String getMapHtml(double lat, double lng,int zoom){
+        return "<html>\n" +
+                "  <head>\n" +
+                "    <script src=\"https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.js\"></script>\n" +
+                "    <link type=\"text/css\" rel=\"stylesheet\" href=\"https://api.mqcdn.com/sdk/mapquest-js/v1.3.2/mapquest.css\"/>\n" +
+                "\n" +
+                "    <script type=\"text/javascript\">\n" +
+                "      window.onload = function() {\n" +
+                "        L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';\n" +
+                "\n" +
+                "        var map = L.mapquest.map('map', {\n" +
+                "          center: ["+lat+", "+lng+"],\n" +
+                "          layers: L.mapquest.tileLayer('map'),\n" +
+                "          zoom: "+zoom+"\n" +
+                "        });\n" +
+                "\n" +
+                "        map.addControl(L.mapquest.control());\n" +
+                "      }\n" +
+                "    </script>\n" +
+                "  </head>\n" +
+                "\n" +
+                "  <body style=\"border: 0; margin: 0;\">\n" +
+                "    <div id=\"map\" style=\"width: 100%; height: 530px;\"></div>\n" +
+                "  </body>\n" +
+                "</html>";
     }
 }
